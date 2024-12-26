b49ae51c2902c9f4337be7c85ced6ab46e66be48
Eyck-Alexander Jentzsch <alex@minres.com>
2024-12-26 12:48:02 +0000

    adds declarations needed for the tcc vm_base to core.h

--- src/core.h
+++ src/core.h
@@ -8,48 +8,35 @@
 #include <iss/vm_types.h>
 
 using namespace iss;
-struct eve_core : public arch_if {
-#pragma pack(push, 1)
-  struct eve_regs {
-    uint8_t A = 0;
-    uint8_t B = 0;
-    uint8_t C = 0;
-    uint8_t D = 0;
-    uint8_t X = 0;
-    uint8_t Y = 0;
-    uint8_t M1 = 0;
-    uint8_t M2 = 0;
-    uint16_t PC = 0;
-    uint16_t SP = 0;
-    bool CY = 0;
-    bool SN = 0;
-    bool ZE = 0;
-    bool OV = 0;
-  } reg;
-#pragma pack(pop)
-
-  std::array<uint8_t, 0xfff> Imem;
-  std::array<uint8_t, 0xffff> Dmem;
-
-  eve_core();
-  ~eve_core();
-  void reset(uint64_t address = 0) override;
-  uint8_t *get_regs_base_ptr() override;
-  std::pair<uint64_t, bool> load_file(std::string name, int type = -1) override;
-  status read(const address_type type, const access_type access,
-              const uint32_t space, const uint64_t addr, const unsigned length,
-              uint8_t *const data) override;
-  status write(const address_type type, const access_type access,
-               const uint32_t space, const uint64_t addr, const unsigned length,
-               const uint8_t *const data) override;
-
-  // core definitions needed to instantiate the vm
-  iss::sync_type needed_sync() const { return iss::NO_SYNC; }
-};
+// needs to be forward declared for structs to be legal
+// struct needs to be before eve_core for phys_addr_t to be known
+struct eve_core;
 
 // the traits struct is needed to instantiate the vm
 template <> struct arch::traits<eve_core> {
-  enum reg_e { A, B, C, D, X, Y, M1, M2, PC, SP, CY, SN, ZE, OV };
+  // members after OV are needed for the tcc vm
+  enum reg_e {
+    A,
+    B,
+    C,
+    D,
+    X,
+    Y,
+    M1,
+    M2,
+    PC,
+    SP,
+    CY,
+    SN,
+    ZE,
+    OV,
+    ICOUNT,
+    NEXT_PC,
+    LAST_BRANCH,
+    TRAP_STATE,
+    PENDING_TRAP,
+    NUM_REGS
+  };
   enum opcode_e {
     MOV,
     MOVI,
@@ -93,12 +80,62 @@ template <> struct arch::traits<eve_core> {
   using code_word_t = uint32_t;
   using virt_addr_t = typed_addr_t<address_type::PHYSICAL>;
   using phys_addr_t = typed_addr_t<address_type::PHYSICAL>;
-  static constexpr std::array<uint32_t, 14> reg_bit_widths{
-      8, 8, 8, 8, 8, 8, 8, 8, 16, 16, 1, 1, 1, 1};
-  static constexpr std::array<uint32_t, 14> reg_byte_offsets{
-      0, 1, 2, 3, 4, 5, 6, 7, 9, 11, 12, 13, 14, 15};
+  static constexpr std::array<uint32_t, 19> reg_bit_widths{
+      8, 8, 8, 8, 8, 8, 8, 8, 16, 16, 1, 1, 1, 1, 8, 2, 1, 1, 1};
+  static constexpr std::array<uint32_t, 19> reg_byte_offsets{
+      0, 1, 2, 3, 4, 5, 6, 7, 9, 11, 12, 13, 14, 15, 16, 20, 22, 23, 24};
   enum sreg_flag_e { FLAGS };
   enum mem_type_e { MEM, IMEM };
 };
 
+struct eve_core : public arch_if {
+#pragma pack(push, 1)
+  struct eve_regs {
+    uint8_t A = 0;
+    uint8_t B = 0;
+    uint8_t C = 0;
+    uint8_t D = 0;
+    uint8_t X = 0;
+    uint8_t Y = 0;
+    uint8_t M1 = 0;
+    uint8_t M2 = 0;
+    uint16_t PC = 0;
+    uint16_t SP = 0;
+    bool CY = 0;
+    bool SN = 0;
+    bool ZE = 0;
+    bool OV = 0;
+    uint64_t ICOUNT;
+    uint16_t NEXT_PC;
+    uint8_t LAST_BRANCH;
+    uint8_t TRAP_STATE;
+    uint8_t PENDING_TRAP;
+  } reg;
+#pragma pack(pop)
+
+  std::array<uint8_t, 0xfff> Imem;
+  std::array<uint8_t, 0xffff> Dmem;
+
+  eve_core();
+  ~eve_core();
+  void reset(uint64_t address = 0) override;
+  uint8_t *get_regs_base_ptr() override;
+  std::pair<uint64_t, bool> load_file(std::string name, int type = -1) override;
+  status read(const address_type type, const access_type access,
+              const uint32_t space, const uint64_t addr, const unsigned length,
+              uint8_t *const data) override;
+  status write(const address_type type, const access_type access,
+               const uint32_t space, const uint64_t addr, const unsigned length,
+               const uint8_t *const data) override;
+
+  // core definitions needed to instantiate the vm
+  iss::sync_type needed_sync() const { return iss::NO_SYNC; }
+
+  // further core definitions needed to instantiate the tcc vm
+  inline bool should_stop();
+  inline uint64_t stop_code();
+  inline uint64_t get_icount();
+  iss::arch::traits<eve_core>::phys_addr_t virt2phys(const iss::addr_t &addr);
+  inline uint32_t get_last_branch();
+};
 #endif
